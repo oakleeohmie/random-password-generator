5d14c8efd09228d52f6d1010e471ed688a34be49
Oaklee Ohmie <oakleeohmie@outlook.com>
2019-11-25 15:04:08 -0700

    my first commit

---
+++ password.js
@@ -0,0 +1,9 @@
+function generate() {
+    var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    var numbers = '0123456789';
+    var symbols = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
+    var all = uppercase + lowercase + numbers + symbols;
+
+    var password = '';
+}
