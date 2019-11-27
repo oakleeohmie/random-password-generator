ca84b14fe436602c84225c724d2a6204fe4f525d
Oaklee Ohmie <oakleeohmie@outlook.com>
2019-11-26 18:25:34 -0700

    modified

--- password.js
+++ password.js
@@ -1,4 +1,4 @@
-function generate() {
+function generate(length = 12) {
     var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     var lowercase = 'abcdefghijklmnopqrstuvwxyz';
     var numbers = '0123456789';
